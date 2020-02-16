799d13a339a77c942e65cf5d83205817ab3634b2
nogizhopaboroda <al.zhukoff@gmail.com>
2020-02-16 15:52:04 +0100

    add buffers

--- broadcaster.go
+++ broadcaster.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bufio"
+	// "encoding/binary"
 	"flag"
 	"log"
 	"os"
+	// "fmt"
+	// "io"
         "./ws_broadcaster"
         "./tcp_broadcaster"
 )
@@ -22,25 +25,46 @@ func Broadcast(){
 }
 
 
-func readSource(fileName string) {
+func readSource(fileName string, bufferSize int) {
+
+
 
 	file, err := os.OpenFile(fileName, os.O_RDONLY, os.ModeNamedPipe)
 	if err != nil {
 		log.Fatal("Open named pipe file error:", err)
 	}
 
+	defer file.Close()
+
 	reader := bufio.NewReader(file)
 
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err == nil {
-                  messages <- []byte(line)
-		}
+
+	//Experimenting with buffers
+	//TODO refactor this somehow
+	if bufferSize == 0 {
+	  for {
+	    line, err := reader.ReadBytes('\n')
+	    if err == nil {
+	      messages <- []byte(line)
+	    }
+	  }
+	} else {
+	  buffer := make([]byte, bufferSize)
+
+	  for {
+	    bytesread, err := reader.Read(buffer)
+
+	    if err == nil {
+	      messages <- []byte(buffer[0:bytesread])
+	    }
+	  }
 	}
+
 }
 
 
 func main() {
+  bufferSize := flag.Int("b", 0, "buffer size (in bytes)")
   wsPortNo := flag.Int("wp", 8080, "websocket server port")
   tcpPortNo := flag.Int("tp", 1235, "tcp server port")
   fileName := flag.String("i", "", "input source")
@@ -51,5 +75,5 @@ func main() {
 
   go Broadcast()
 
-  readSource(*fileName)
+  readSource(*fileName, *bufferSize)
 }
